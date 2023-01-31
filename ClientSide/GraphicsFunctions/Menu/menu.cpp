936054793bd12289decd9b0b8224eeaffd134edb
Oplp25 <jakobwood25@gmail.com>
2023-01-31 20:55:53 +0000

    Update menu.cpp

--- ClientSide/GraphicsFunctions/Menu/menu.cpp
+++ ClientSide/GraphicsFunctions/Menu/menu.cpp
@@ -8,11 +8,11 @@ using namespace colours;
 void runMenu(sf::RenderWindow& win) {
 	int width = win.getSize().x;
 	int height = win.getSize().y;
-	sf::RectangleShape dungeonButton(sf::Vector2f(width/2,height/5));
-	dungeonButton.setPosition(sf::Vector2f(width/4, 7 * height/10));
+	sf::RectangleShape dungeonButton(sf::Vector2f(width/2,height/9));
+	dungeonButton.setPosition(sf::Vector2f(width/4, 7 * height/9));
 	dungeonButton.setFillColor(cinereous);
-	sf::RectangleShape arenaButton(sf::Vector2f(width / 2,  height / 5));
-	arenaButton.setPosition(sf::Vector2f(width / 4, 2 * height / 5));
+	sf::RectangleShape arenaButton(sf::Vector2f(width / 2,  height / 9));
+	arenaButton.setPosition(sf::Vector2f(width / 4, 5 * height / 9));
 	arenaButton.setFillColor(cinereous);
 	vector<sf::RectangleShape> buttonList = {arenaButton,dungeonButton};
 	sf::Event event;
