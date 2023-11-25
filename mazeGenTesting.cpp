814e1257e1e741387cdef32647d00a8012851148
Oplp25 <jakobwood25@gmail.com>
2023-11-25 18:00:02 +0000

    changed symbols

--- mazeGenTesting.cpp
+++ mazeGenTesting.cpp
@@ -3,43 +3,47 @@
 #include <string>
 #include <unordered_map>
 #include <random>
-
+#include <fcntl.h>
 using namespace std;
 
-char h2 = '-';
-char h3d = 'T';
-char h3u = '⊥';
-char c1 = '⌜';
-char c2 = '⌝';
-char c3 = '⌟';
-char c4 = '⌞';
-char h4 = '+';
-char v2 = '|';
-char v3r = '⊢';
-char v3l = '⊣';
-unordered_map<string,char> cellChars = {{"h2",h2},{"h3d",h3d},{"h3u",h3u},{"c1",c1},{"c2",c2},{"c3",c3},{"c4",c4},{"h4",h4},{"v2",v2},{"v3r",v3r},{"v3l",v3l}};
+string h2 = "---";
+string h3d = " T ";
+string h3u = "_|_";
+string c1 = "|^-";
+string c2 = "¬";
+string c3 = "__|";
+string c4 = "L__";
+string h4 = "-|-";
+string v2 = " | ";
+string v3r = "|--";
+string v3l = "--|";
+unordered_map<string,string> cellstrings = {{"h2",h2},{"h3d",h3d},{"h3u",h3u},{"c1",c1},{"c2",c2},{"c3",c3},{"c4",c4},{"h4",h4},{"v2",v2},{"v3r",v3r},{"v3l",v3l}};
 unordered_map<string,vector<int>> cellConns={{"h2",{2,4}},{"h3d",{2,3,4}},{"h3u",{1,2,4}},{"c1",{1,4}},{"c2",{1,2}},{"c3",{2,3}},{"c4",{3,4}},{"h4",{1,2,3,4}},{"v2",{1,3}},{"v3r",{1,2,3}},{"v3l",{1,3,4}}};
-unordered_map<vector<int>,string> cellConnsReverse={{{2,4},"h2"},{{2,3,4},"h3d"},{{1,2,4},"h3u"},{{1,4},"c1"},{{1,2},"c2"},{{2,3},"c3"},{{3,4},"c4"},{{1,2,3,4},"h4"},{{1,3},"v2"},{{1,2,3},"v3r"},{{1,3,4},"v3l"}};
-vector<vector<int>> combos = {{1,2},{1,3},{1,4},{2,3}{2,4},{3,4},{1,2,3},{1,2,4},{1,3,4},{2,3,4},{1,2,3,4}};
+//unordered_map<vector<int>,string> cellConnsReverse={{{2,4},"h2"},{{2,3,4},"h3d"},{{1,2,4},"h3u"},{{1,4},"c1"},{{1,2},"c2"},{{2,3},"c3"},{{3,4},"c4"},{{1,2,3,4},"h4"},{{1,3},"v2"},{{1,2,3},"v3r"},{{1,3,4},"v3l"}};
+vector<string> cellTypes = {"h2","h3d","h3u","c1","c2","c3","c4","h4","v2","v3r","v3l"};
+vector<vector<int>> combos = {{1,2},{1,3},{1,4},{2,3},{2,4},{3,4},{1,2,3},{1,2,4},{1,3,4},{2,3,4},{1,2,3,4}};
+vector<vector<int>> order = {{2,3},{1,3},{1,2},{1,1},{2,1},{3,1},{3,2},{3,3},{3,4},{2,4},{1,4},{0,4},{0,3},{0,2},{0,1},{0,0},{1,0},{2,0},{3,0},{4,0},{4,1},{4,2},{4,3},{4,4}};
+//unordered_map<vector<int>,int> posEnum = {{vector<int>(0,1),1},{vector<int>(1,0),2},{vector<int>(0,-1),3},{vector<int>(-1,0),4}};
+
 class cell{
     private:
-        char symbol;
+        string symbol;
     public:
         vector<int> conns;// 1-up, 2-right, 3-down, 4-left
         vector<int> pos;//sf::Vector2f pos;
         string cellType;
-        char draw(){
+        string draw(){
             return symbol;
         }
         string genNewCell(int pos){
             if(count(conns.begin(),conns.end(),pos)==0){
                 return "n";
             }
-            vector<string> connsList1;
+            vector<vector<int>> connsList1;
             //create list of all cell types that connect with the cell on pos side
             for(vector<int> i:combos){
-                if(count(i.begin(),i.end(),pos!=0)){
-                    connsList1.push_back(cellConnsReverse[i]);
+                if(count(i.begin(),i.end(),pos)!=0){
+                    connsList1.push_back(i);
                 }
             }
             //select random item from list
@@ -47,29 +51,79 @@ class cell{
 	        uniform_int_distribution<int> distX(1, connsList1.size());
 	        randGen.seed(time(0));
 	        int rolled = distX(randGen);
+            vector<int> rtn = connsList1[rolled];
+            for(string i:cellTypes){
+                if(cellConns[i] == rtn){
+                    return i;
+                }
+            }
+            return "h4";
             //return that string
-            return connsList1[rolled];
         }
-        cell(string cType){
+        cell(string cType, vector<int> posP){
+            cout<<cType<<endl;
             cellType = cType;
-            symbol = cellChars[cType];
+            symbol = cellstrings[cType];
             conns = cellConns[cType];
+            pos = posP;
         }
         cell(){
-            cellType = "h2";
-            symbol = '-';
+            pos = {5,5};
+            cellType = "e";
+            symbol = "OOO";
             conns = {2,4};
         }
 };
 int main(){
-    cout<<combos.size()<<endl;
-    cout<<cellConns.size()<<endl;
-    vector<vector<cell>> cellList;
-    for(int i=0;i<6;i++){
-        for(int j=0;j<6;j++){
-
+    default_random_engine randGen;
+	randGen.seed(time(0));
+	cell aCell;
+    vector<vector<cell>> cellList = {{cell(),cell(),cell(),cell(),cell()},{cell(),cell(),cell(),cell(),cell()},{cell(),cell(),cell(),cell(),cell()},{cell(),cell(),cell(),cell(),cell()},{cell(),cell(),cell(),cell(),cell()}};
+    vector<cell> chooseList;
+    cellList[2][2]=cell("h4",{2,2});
+    for(vector<int> i:order){
+        chooseList.clear();
+        vector<int> a;
+        vector<int> b;
+        vector<int> c;
+        vector<int> d;
+        for(int j = 0;j<5;j++){
+            for(int k = 0;k<5;k++){
+                a = {j,k+1};
+                b = {j,k-1};
+                c = {j+1,k};
+                d = {j-1,k};
+                if (a==i || b==i || c==i || d==i && cellList[j][k].cellType!="e"){
+                    chooseList.push_back(cellList[j][k]);
+                }
+            }
         }
-
+        uniform_int_distribution<int> distX(0, chooseList.size()-1);
+        int rolled = distX(randGen);
+        aCell = chooseList[rolled];
+        //int relation = posEnum[{i[0]-aCell.pos[0],i[1]-aCell.pos[1]}];
+        int relation;
+        cout<<aCell.pos[0]<<" , "<<aCell.pos[1]<<endl;
+        vector<int> comp = {i[0]-aCell.pos[0],i[1]-aCell.pos[1]};
+        if(comp == vector<int>(0,1)){
+            relation = 1;
+        } else if(comp == vector<int>(1,0)){
+            relation = 2;
+        }else if(comp == vector<int>(0,-1)){
+            relation = 3;
+        } else{
+            relation = 4;
+        }
+        cellList[i[0]][i[1]] = cell(aCell.genNewCell(relation),i);
+        //cellList[i[0]][i[1]] = cell("h3u",i);
     }
+    for (vector<cell> i:cellList){
+        for(cell j:i){
+            cout<<j.draw();
+            //cout<<h3u;
+        }
+        cout<<endl;
+    }
+    
     return 0;
 }
