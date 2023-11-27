b806c30f6c61df2ae94d869fdf6642be0f02b640
Oplp25 <jakobwood25@gmail.com>
2023-11-27 22:52:19 +0000

    Bugs in code fixed

--- mazeGenTesting.cpp
+++ mazeGenTesting.cpp
@@ -12,7 +12,7 @@ vector<string> h3d = {"______","__  __"," |  | "};
 
 vector<string> h3u = {"_|  |_","______","      "};
 
-vector<string> c1 = {" ____"," |  _"," |  | "};
+vector<string> c1 = {" _____"," |  __"," |  | "};
 
 vector<string> c2 = {"_____ ","__  | "," |  | "};
 
@@ -26,14 +26,12 @@ vector<string> v2 = {" |  | "," |  | "," |  | "};
 
 vector<string> v3r = {" |  |_"," |   _"," |  | "};
 
-vector<string> v3l = {"_|  | ","_   | "," |  | "};//11 symbols
+vector<string> v3l = {"_|  | ","__  | "," |  | "};//11 symbols
 unordered_map<string,vector<string>> cellstrings = {{"h2",h2},{"h3d",h3d},{"h3u",h3u},{"c1",c1},{"c2",c2},{"c3",c3},{"c4",c4},{"h4",h4},{"v2",v2},{"v3r",v3r},{"v3l",v3l}};
-unordered_map<string,vector<int>> cellConns={{"h2",{2,4}},{"h3d",{2,3,4}},{"h3u",{1,2,4}},{"c1",{1,4}},{"c2",{1,2}},{"c3",{2,3}},{"c4",{3,4}},{"h4",{1,2,3,4}},{"v2",{1,3}},{"v3r",{1,2,3}},{"v3l",{1,3,4}}};
-//unordered_map<vector<int>,string> cellConnsReverse={{{2,4},"h2"},{{2,3,4},"h3d"},{{1,2,4},"h3u"},{{1,4},"c1"},{{1,2},"c2"},{{2,3},"c3"},{{3,4},"c4"},{{1,2,3,4},"h4"},{{1,3},"v2"},{{1,2,3},"v3r"},{{1,3,4},"v3l"}};
+unordered_map<string,vector<int>> cellConns={{"h2",{2,4}},{"h3d",{2,3,4}},{"h3u",{1,2,4}},{"c1",{2,3}},{"c2",{3,4}},{"c3",{1,4}},{"c4",{1,2}},{"h4",{1,2,3,4}},{"v2",{1,3}},{"v3r",{1,2,3}},{"v3l",{1,3,4}}};
 vector<string> cellTypes = {"h2","h3d","h3u","c1","c2","c3","c4","h4","v2","v3r","v3l"};
 vector<vector<int>> combos = {{1,2},{1,3},{1,4},{2,3},{2,4},{3,4},{1,2,3},{1,2,4},{1,3,4},{2,3,4},{1,2,3,4}};
 vector<vector<int>> order = {{2,3},{1,3},{1,2},{1,1},{2,1},{3,1},{3,2},{3,3},{3,4},{2,4},{1,4},{0,4},{0,3},{0,2},{0,1},{0,0},{1,0},{2,0},{3,0},{4,0},{4,1},{4,2},{4,3},{4,4}};
-//unordered_map<vector<int>,int> posEnum = {{vector<int>(0,1),1},{vector<int>(1,0),2},{vector<int>(0,-1),3},{vector<int>(-1,0),4}};
 
 default_random_engine randGen;
 
@@ -48,23 +46,46 @@ class cell{
         string draw(int i){
             return symbol[i];
         }
-        string genNewCell(int pos){
-            if(count(conns.begin(),conns.end(),pos)==0){
-                return "n";
+        string genNewCell(int relation){
+            int relop;//opposite side
+            switch( relation){
+                case 1:relop = 3;break;
+                case 2:relop = 4;break;
+                case 3:relop = 1;break;
+                case 4:relop = 2;break;
+            };
+            vector<int> connst = conns;
+            connst.push_back(0);
+            cout<<"connst: ";
+            for(int i: connst){
+                cout<<i<<" , ";
+            }cout<<endl;
+            cout<<"Relation: "<<relation<<endl;
+            cout<<"Relop: "<<relop<<endl;
+            if(find(connst.begin(),connst.end(),relation) == connst.end()){
+                return "h4";// instead, could try and find another cell it is ajacent to
             }
             vector<vector<int>> connsList1;
             //create list of all cell types that connect with the cell on pos side
+            vector<int> j;
             for(vector<int> i:combos){
-                if(count(i.begin(),i.end(),pos)!=0){
+                j = i;
+                j.push_back(0);
+                if(find(j.begin(),j.end(),relop)!= j.end()){
                     connsList1.push_back(i);
                 }
             }
+            cout<<"connsList1: ";
+            for(vector<int> i: connsList1){
+                cout<<"("<<i[0]<<" , "<<i[1]<<")"<<" , ";
+            }cout<<endl;
             //select random item from list
 	        uniform_int_distribution<int> distX(1, connsList1.size());
 	        int rolled = distX(randGen);
             vector<int> rtn = connsList1[rolled];
             for(string i:cellTypes){
                 if(cellConns[i] == rtn){
+                    cout<<"cellType: "<<i<<endl;
                     return i;
                 }
             }
@@ -87,7 +108,7 @@ class cell{
 int main(){
 	randGen.seed(time(0));
 	cell aCell;
-    vector<vector<cell>> cellList = {{cell(),cell(),cell(),cell(),cell()},{cell(),cell(),cell(),cell(),cell()},{cell(),cell(),cell(),cell(),cell()},{cell(),cell(),cell(),cell(),cell()},{cell(),cell(),cell(),cell(),cell()}};
+    vector<vector<cell>> cellList = {{cell(),cell(),cell(),cell(),cell()},{cell(),cell(),cell(),cell(),cell()},{cell(),cell(),cell(),cell(),cell()},{cell(),cell(),cell(),cell(),cell()},{cell(),cell(),cell(),cell(),cell()}};//cellList[y][x]
     vector<cell> chooseList;
     cellList[2][2]=cell("h4",{2,2});
     for(vector<int> i:order){
@@ -123,16 +144,17 @@ int main(){
         cout<<"I: "<<i[0]<<" , "<<i[1]<<endl;
         cout<<"acell: "<<aCell.pos[0]<<" , "<<aCell.pos[1]<<endl;
         vector<int> comp = {i[0]-aCell.pos[0],i[1]-aCell.pos[1]};
-        if(comp == vector<int>(0,1)){
-            relation = 1;
-        } else if(comp == vector<int>(1,0)){
+        if(comp [0] == 0 && comp[1] == 1){//i is to the right of aCell
             relation = 2;
-        }else if(comp == vector<int>(0,-1)){
+        } else if(comp[0] == 1 && comp[1] == 0){//i is below of aCell
             relation = 3;
-        } else{
+        }else if(comp[0] == 0 && comp[1] == -1){//i is to the left of aCell
             relation = 4;
+        } else{//i is above aCell 
+            relation = 1;
         }
         cellList[i[0]][i[1]] = cell(aCell.genNewCell(relation),i);
+        cout<<endl<<"------------------------------------------"<<endl<<endl;
     }
     for (vector<cell> i:cellList){
         for(cell k:i){
