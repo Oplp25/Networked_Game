322ed324fc7cb1f8308832962b241b80bed05de1
Oplp25 <78849187+Oplp25@users.noreply.github.com>
2024-04-18 18:48:57 +0100

    Added Comments

--- ClientSide/testFolder(Temp)/clientTest.cpp
+++ ClientSide/testFolder(Temp)/clientTest.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 void getConnection(sf::TcpSocket &socket, sf::IpAddress address , int port)
 {
-	sf::Socket::Status status = socket.connect(address, port);
-	if (status != sf::Socket::Done){
+	sf::Socket::Status status = socket.connect(address, port);// attempts to connect to port port at IP address address
+	if (status != sf::Socket::Done){//If it failed
 		cout << "Error. Client could not connect to server." << endl;
 		throw std::invalid_argument("Socket could not connect");
 	}
@@ -13,8 +13,9 @@ void getConnection(sf::TcpSocket &socket, sf::IpAddress address , int port)
 
 void sendMessage(sf::TcpSocket& socket, std::string message)
 {
-	if (socket.send(message.c_str(), message.size()+1) != sf::Socket::Done)
+	if (socket.send(message.c_str(), message.size()+1) != sf::Socket::Done)//converts message to a C-type string. Also have to pass the size of the message being sent.
 	{
+		//if it failed
 		cout << "Error. Message not sent." << endl;
 		throw std::invalid_argument("Message not sent");
 	}
